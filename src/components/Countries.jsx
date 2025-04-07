90b6cebe43f8405aa31b40d3e446a3f48b394309
ArifRehman-Info <asustufk513ea@gmail.com>
2025-04-07 10:31:37 +0600

    Countries-Basic-Conditional-Load

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,18 +1,22 @@
 import React, { use } from 'react';
 import Country from './Countries/Country/Country';
+ import './Countries.css'
 
 const Countries = ({countriesPromise}) => {
 
     const countries = use(countriesPromise);
 
-    console.log(countries);
+    // console.log(countries);
     return (
         <div>
             <h2>Traveling Countries:{countries.length}</h2>
+
+            <div className='countries'>
             {
 
                 countries.map(country => <Country key={country.cca3} country ={country}></Country>)
             }
+            </div>
         </div>
     );
 };
--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css'
 
 const Country = ({country}) => {
 
-    console.log(country);
+    const [visited, setVisited]= useState(false);
+
+    const countVisited = () => {
+      
+        setVisited(true);
+        
+    }
+
     return (
-        <div>
+        <div className={`country ${visited && 'countries-visited'}`}>
         <h3>Name: {country.name.common} </h3>
         {/* we can't use like this img src ="inside for dynamic one" */}
         <img src={country.flags.png} alt="" /> 
         <p>Independent: {country.independent ? 'Free': 'Not Free'}</p>
         <p>Population: {country.population} </p>
+        <button onClick={countVisited}>{visited? 'Visited': 'Not Visited'}</button>
        
         </div>
     );
